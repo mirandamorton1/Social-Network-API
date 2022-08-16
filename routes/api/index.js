46f1d805bff40ca7cc3d3246d0be66eca0ce73d7
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-16 18:00:56 -0500

    index added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,13 @@
 // bring in router from express
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 // bring in routes
 
 // set up routes using router.use
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 // export
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 // express
+const express = require('express');
 // db from config
+const db = require('./config/connection');
 // routes from routes
+const routes = require('./routes');
 
 // port
+const PORT = process.env.PORT || 3001;
+const app = express();
 // app
 
 // app.use for url encoded
+app.use(express.urlencoded({ extended: true }));
 // app.use for express.json
+app.use(express.json());
 // app.use for routes
+app.use(routes);
 
 //set up app to listen ocne db is open
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
