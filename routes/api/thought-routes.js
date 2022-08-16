5a5ed309b26472777223f1053c7a1df34a362ce4
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-16 18:18:46 -0500

    more routes added

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,12 @@
+// same as other route
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} =  require('../../controllers/thoughtController');
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,26 @@
 // router from express
+const router = require('express').Router();
+
 const {
 // all of your user controllers
-    getUsers
-} = // required from controllers
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+
+} = require('../../controllers/user-controller') // required from controllers
 
 // set up routes
-router.route('/').get(getUsers).post(createUser)
+router.route('/').get(getUsers).post(createUser);
+
+// api/users/:userId - activity 23
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 //export
+module.exports = router;
 
