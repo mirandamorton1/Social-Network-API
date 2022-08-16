c1087af0bb23ea137ed8e720549b6c45b741e15b
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-16 17:34:32 -0500

    thought model added

--- models/Thought.js
+++ models/Thought.js
@@ -1,10 +1,46 @@
 // similar pattern to User
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
 
 // pull in the reactionSchema from reaction model
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJson: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
 
 // have an array of rection schema
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
-reactions: [reactionSchema]
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
+
 
 //virtual for reactionCount
 
