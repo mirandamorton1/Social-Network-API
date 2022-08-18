fcb0999c8043fab10745b993cb94b37c5c3f57f8
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-17 21:17:45 -0500

    thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,12 +1,114 @@
 // bring in your models
+const { User, Thought } = require("../models");
 
 // set up controllers
+module.exports = {
+    //get all thoughts
+    //thought.find
+    getThought(req, res) {
+        Thought.find()
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
+    },
+    // get single thought by id
+    // thought.findOne
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with taht ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 
-// get all thoughs
-//thought.find
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $addToSet: { thought: thought._id } },
+                    { new: true }
+                );
+            })
+            .then((thought) => 
+            !thought
+                ? res.status(404).json({ message: 'No thought found with this ID',
+            })
+            : res.json('Created the thought')
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
 
-// get single though by id
-// thought.findOnce
+    updateThought(req, res) {
+        Thought.fineOneAndUpdate(
+            { _id: req.params.videoId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with this ID' })
+                    : res.json(thought)
+                )
+                .catch((err) => res.status(500).json(err));
+    },
+
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id:req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with this ID' })
+                    : User.findOneAndUpdate(
+                        { thought: req.params.thoughtId },
+                        { $pull: { thought: req.params.thoughtId } },
+                        { new: true }
+                    )
+            )
+            .then((user) => 
+            !user
+                ? res.status(404)
+                .json({ message: 'Thought deleted but no user with this id!' })
+                : res.json({ message: 'Thought successfully deleted!' })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runvalidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought found with this id'})
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+            !thought
+            ? res.status(404).json({ message: 'No thought found with this ID' })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+};
+
+
+
 
 //create a though
 //Thought.creat
